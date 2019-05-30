24faaac338273414cc7cb1d21c54afea355e20cd
tzing <tzingshih@gmail.com>
2019-05-30 23:23:46 +0800

    [WIP] add views & templates

--- tsiahpng/views.py
+++ tsiahpng/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import uuid
 
-# Create your views here.
+from django.shortcuts import render
+from django.urls import path
+
+from django.views.i18n import JavaScriptCatalog
+from django.views.decorators.cache import cache_page
+
+
+from . import admin
+
+app_name = "tsiahpng"
+caches = cache_page(86400, key_prefix=f"jsi18n-{uuid.uuid4().hex}")
+
+urlpatterns = [
+    path(
+        "jsi18n/",
+        caches(JavaScriptCatalog.as_view(packages=["tsiahpng"])),
+        name="javascript-catalog",
+    ),
+    path("administration/", admin.site.urls),
+]
