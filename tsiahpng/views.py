1e572dfe5670158478288e3f6d0a2a73cfd1902c
tzing <tzingshih@gmail.com>
2019-05-31 00:05:20 +0800

    refactor base layout

--- tsiahpng/views.py
+++ tsiahpng/views.py
@@ -8,10 +8,17 @@ from django.views.decorators.cache import cache_page
 
 from . import admin
 
+
+def welcome(request):
+    return render(request, "tsiahpng/homepage.pug")
+
+
+# url confs
 app_name = "tsiahpng"
 caches = cache_page(86400, key_prefix=f"jsi18n-{uuid.uuid4().hex}")
 
 urlpatterns = [
+    path("", welcome, name="welcome"),
     path(
         "jsi18n/",
         caches(JavaScriptCatalog.as_view(packages=["tsiahpng"])),
