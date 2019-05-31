d9a31ba9f1047a09e3e0e1ab28e8490085b93e6e
tzing <tzingshih@gmail.com>
2019-05-31 16:29:25 +0800

    create empty page for add products

--- tsiahpng/views.py
+++ tsiahpng/views.py
@@ -1,8 +1,10 @@
 import random
 import uuid
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import path
+
+from django.contrib import messages
 
 from django.utils.translation import gettext as _
 
@@ -34,17 +36,56 @@ def shop_list(request):
     )
 
 
+def shop_detail(request):
+    # get shop
+    try:
+        shop = models.Shop.objects.get(id=shop_id)
+    except models.Shop.DoesNotExist:
+        messages.error(request, _("Shop not exists."))
+        return redirect("tsiahpng:welcome")
+
+    raise NotImplementedError()  # FIXME
+
+
+def shop_add_product(request, shop_id):
+    # get shop
+    try:
+        shop = models.Shop.objects.get(id=shop_id)
+    except models.Shop.DoesNotExist:
+        messages.error(request, _("Shop not exists."))
+        return redirect("tsiahpng:welcome")
+
+    # check permission
+    if not shop.changeable:
+        messages.error(request, _("{shop} is not changeable.").format(shop=shop))
+        return redirect("tsiahpng:shop_detail", shop_id=shop_id)
+
+    raise NotImplementedError()  # FIXME
+
+    # render
+    return render(
+        request,
+        "tsiahpng/menu/add_product.pug",
+        {"title": _("Add product to {shop}".format(shop=shop))},
+    )
+
+
 # url confs
 app_name = "tsiahpng"
 caches = cache_page(86400, key_prefix=f"jsi18n-{uuid.uuid4().hex}")
 
 urlpatterns = [
     path("", homepage, name="welcome"),
+    # menu
     path("menu/", shop_list, name="shop_list"),
+    path("menu/<int:shop_id>", shop_detail, name="shop_detail"),
+    path("menu/<int:shop_id>/add", shop_add_product, name="shop_add_product"),
+    # js i18n
     path(
         "jsi18n/",
         caches(JavaScriptCatalog.as_view(packages=["tsiahpng"])),
         name="javascript-catalog",
     ),
+    # admin panel
     path("administration/", admin.site.urls, name="admin"),
 ]
