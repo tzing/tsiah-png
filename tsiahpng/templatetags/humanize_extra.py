e46250e64807d4aa2eab22390488778bedf07179
tzing <tzingshih@gmail.com>
2019-06-06 16:44:06 +0800

    extend view for order details

--- tsiahpng/templatetags/humanize_extra.py
+++ tsiahpng/templatetags/humanize_extra.py
@@ -2,7 +2,10 @@ from datetime import date, datetime
 
 import django.template
 
+from django.db.models import Sum
 from django.utils.translation import gettext as _
+
+from .. import models
 
 register = django.template.Library()
 
@@ -31,3 +34,35 @@ def short_naturalday(value):
         return django.template.defaultfilters.date(value, "n/j")
 
     return django.template.defaultfilters.date(value, "%Y/%n/%j")
+
+
+@register.filter()
+def percategory_quantity(order):
+    assert isinstance(order, models.Order)
+    # get products
+    product_ids = order.tickets().values_list("item").order_by().distinct()
+    products = models.Product.objects.filter(id__in=product_ids)
+
+    # get categories
+    category_ids = products.values_list("category").order_by().distinct()
+    categories = models.Category.objects.filter(id__in=category_ids)
+
+    # counting
+    counts = []
+    for cat in categories:
+        related_products = products.filter(category=cat)
+        sum_qty = order.tickets(item__in=related_products).aggregate(
+            val=Sum("quantity")
+        )["val"]
+        if sum_qty:
+            counts.append(_("{cat} ×{qty}").format(cat=cat, qty=sum_qty))
+
+    unsorted_products = products.filter(category=None)
+    if unsorted_products:
+        sum_qty = order.tickets(item__in=unsorted_products).aggregate(
+            val=Sum("quantity")
+        )["val"]
+        counts.append(_("{cat} ×{qty}").format(cat=_("Unsorted"), qty=sum_qty))
+
+    # join string
+    return _(", ").join(counts)
