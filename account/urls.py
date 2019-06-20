81428c199e36ed279db2e4ae9beebbc1eb004303
tzing <tzingshih@gmail.com>
2019-06-20 23:41:27 +0800

    fix i18n on accounting app; revise translation

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,18 @@
+import uuid
+
 from django.urls import path, include
+from django.views.decorators.cache import cache_page
+from django.views.i18n import JavaScriptCatalog
 
 from . import views
 
 app_name = "account"
 
+caches = cache_page(86400, key_prefix=f"jsi18n-{uuid.uuid4().hex}")
+
 urlpatterns = [
     path("", views.account_list, name="list"),
     path("<int:passbook_id>/", views.account_detail, name="detail"),
     path("<int:passbook_id>/add", views.create_event, name="create_event"),
+    path("jsi18n/", caches(JavaScriptCatalog.as_view()), name="javascript-catalog"),
 ]
