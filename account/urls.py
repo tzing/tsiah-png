e0951d64718fd05663d1bb9b559e2b78130202f4
tzing <tzingshih@gmail.com>
2019-07-02 22:05:52 +0800

    remove duplicated i18n import

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,8 @@
-import uuid
-
 from django.urls import path, include
-from django.views.decorators.cache import cache_page
-from django.views.i18n import JavaScriptCatalog
 
 from . import views
 
 app_name = "account"
-
-caches = cache_page(86400, key_prefix=f"jsi18n-{uuid.uuid4().hex}")
 
 urlpatterns = [
     path("", views.account_list, name="list"),
@@ -16,5 +10,4 @@ urlpatterns = [
     path("<int:passbook_id>/add", views.create_event, name="create_event"),
     path("from-order/<int:order_id>", views.order_close, name="order_close"),
     path("api/accounts", views.account_list_api, name="api-list"),
-    path("jsi18n/", caches(JavaScriptCatalog.as_view()), name="javascript-catalog"),
 ]
